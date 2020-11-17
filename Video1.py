4ae11f5128f06da16451368a505540ecdd29a37a
Borge122 <gbird1972@hotmail.co.uk>
2020-11-17 19:38:08 +0000

    Update

--- Video1.py
+++ Video1.py
@@ -56,38 +56,41 @@ class s3(GraphScene):
         "y_max": 4,
         "graph_origin": ORIGIN,
         "function_color": WHITE,
-        "axes_color": BLUE
+        "axes_color": BLUE,
+        "x_axis_label": "$x$",
+        "y_axis_label": "$y$"
     }
     def construct(self):
         #Make graph
         self.setup_axes(animate=True)
-        func_graph=self.get_graph(self.func_to_graph,self.function_color)
-        graph_lab = self.get_graph_label(func_graph, label = "x^{2}")
+        self.w = 1
+        self.b = 2
+        ymc = TexMobject("y=m\\times x+c").move_to(3*RIGHT+2*UP)
+        awb = TexMobject("a_1=w\\times a_0+b").move_to(3*RIGHT+2*UP)
+        func_graph = self.get_graph(self.func_to_graph, self.function_color)
+        #graph_lab = self.get_graph_label(func_graph, label = "line") 
 
-        func_graph_2=self.get_graph(self.func_to_graph_2,self.function_color)
-        graph_lab_2 = self.get_graph_label(func_graph_2, label = "x^{3}")
 
-        vert_line = self.get_vertical_line_to_graph(1,func_graph,color=YELLOW)
+        point = Dot(self.coords_to_point(1, 1))
+       
 
-        x = self.coords_to_point(1, self.func_to_graph(1))
-        y = self.coords_to_point(0, self.func_to_graph(1))
-        horz_line = Line(x,y, color=YELLOW)
+        #x = self.coords_to_point(1, self.func_to_graph(1))
+        #y = self.coords_to_point(0, self.func_to_graph(1))
+        #horz_line = Line(x,y, color=YELLOW)
 
-        point = Dot(self.coords_to_point(1,self.func_to_graph(1)))
+        #point = Dot(self.coords_to_point(1,self.func_to_graph(1)))
 
-        #Display graph
-        self.play(ShowCreation(func_graph), Write(graph_lab))
+        #Display graphs
+        self.play(Write(ymc), ShowCreation(func_graph))
+        self.CONFIG["x_axis_label"] = "$a_0$"
         self.wait(1)
-        self.play(ShowCreation(vert_line))
-        self.play(ShowCreation(horz_line))
-        self.add(point)
+        self.play(ReplacementTransform(ymc, awb), self.CONFIG["y_axis_label"], "$a_1$")
         self.wait(1)
-        self.play(Transform(func_graph, func_graph_2), Transform(graph_lab, graph_lab_2))
-        self.wait(2)
+        #self.play(ShowCreation(vert_line))
+        #self.play(ShowCreation(horz_line))
+        #self.add(point)
     def func_to_graph(self, x):
-        return (x**2)
-    def func_to_graph_2(self, x):
-        return(x**3)
+        return self.w*x+self.b
 
 
 class s4(Scene):
